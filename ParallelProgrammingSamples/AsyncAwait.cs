2841ac6fdc2d6cc0c4a0066b9e1d8c9f6cd4c601
Filip Mrhal <filip.mrhal@praguelabs.com>
2022-09-23 14:17:21 +0200

    Internal - Added AsyncAwait class to show basics of the synchronization.

---
+++ ParallelProgrammingSamples/AsyncAwait.cs
@@ -0,0 +1,52 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelProgrammingSamples;
+
+public class AsyncAwait
+{
+    public async Task TestAsync()
+    {
+        await TestWithReturnValueAsync();
+    }
+
+    public async void TestAsyncVoid()
+    {
+        var currentcontext = TaskScheduler.Current;
+    }
+
+    public async Task<int> TestWithReturnValueAsync()
+    {
+        return 0;
+    }
+}
+
+public class MySynchroContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object state)
+    {
+        //Here we can for example limit the amount of concurrent executions. 
+        base.Post(d, state);
+    }
+}
+
+public class ConfigureAwaitFalse
+{
+    public async Task ConfigureAwaitFalseTest()
+    {
+        var aa = new AsyncAwait();
+        await aa.TestAsync().ConfigureAwait(false);
+        aa.TestAsync().GetAwaiter().GetResult();
+        aa.TestAsync().Wait();
+        Task.Run(() => { return;}).RunSynchronously();
+    }
+}
+
+// public struct MyAsyncStateMachineMethodBuilder : AsyncTaskMethodBuilder
+// {
+//     public MyAsyncStateMachineMethodBuilder()
+//     {
+//         
+//     }
+// }
